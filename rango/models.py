1f74edbe221a03a0002def10c3d09810035ed04f
Yaoi Tseng <2229821t@student.gla.ac.uk>
2017-01-26 19:57:49 +0000

    version 26012017

--- rango/models.py
+++ rango/models.py
@@ -5,8 +5,23 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default = 0)
+    likes = models.IntegerField(default = 0)
+    class Meta:
+        verbose_name_plural = "Categories"
 
     #For Python 2, use __unicode__ too
+    def __str__(self):
+        return self.name
+
+class Subject(models.Model):
+    name = models.CharField(max_length=128, unique = True)
+    course = models.CharField(max_length=128, unique = True)
+
+
+    class Meta:
+        verbose_name_plural = "Subject"
+
     def __str__(self):
         return self.name
 
@@ -20,3 +35,4 @@ class Page(models.Model):
     # For Python 2, use __unicode__ too
     def __str__(self):
         return self.title
+
