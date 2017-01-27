644a014b40049855ff1898cda1c38ef045f76ae1
Yaoi Tseng <2229821t@student.gla.ac.uk>
2017-01-27 17:12:44 +0000

    Version chapter 6

--- rango/models.py
+++ rango/models.py
@@ -1,24 +1,10 @@
 from __future__ import unicode_literals
 from django.template.defaultfilters import slugify
 from django.db import models
-from rango.models import page
 
 slug = models.SlugField(blank=True)
 slug = models.SlugField(unique=True)
 # Create your models here.
-
-def show_category(request, category_name_slug):
-    context_dict = {}
-
-    try:
-        category = Category.objects.get(slug = category_name_slug)
-        pages = Page.objects.filter(category=category)
-        context_dict['pages']=pages
-        context_dict['category']=category
-    except Category.DoesNotExist:
-        context_dict['category']=None
-        context_dict['pages']=None
-    return render(request, 'rango/category.html', context_dict)
 
 
 class Category(models.Model):
@@ -55,6 +41,11 @@ class Page(models.Model):
     title = models.CharField(max_length = 128)
     url = models.URLField()
     views = models.IntegerField(default = 0)
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     # For Python 2, use __unicode__ too
     def __str__(self):
--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^about/',views.about,name='about'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
+    url(r'^page/(?P<page_name_slug>[\w\-]+)/$',
+        views.show_page,name='show_page')
+
 ]
