93da2f5bcd40e865ea41d5e9a7bcf0a03533ad30
Yaoi Tseng <2229821t@student.gla.ac.uk>
2017-02-09 18:08:17 +0000

    Version 09.02.2017

--- populate_rango.py
+++ populate_rango.py
@@ -6,12 +6,15 @@ import django
 django.setup()
 from rango.models import Category, Page
 
+
+
+
 def populate():
 
     python_pages = [
         {"title": "Official Python Tutorial",
 		"url": "http://docs.python.org/2/tutorial",
-		"views":0},
+		"views": 0},
         {"title": "How to Think like a Computer Scientist",
          "url": "http://www.greenteapress.com/thinkpython/",
 		 "views":99},
@@ -22,7 +25,7 @@ def populate():
     django_pages = [
         {"title": "Official Django Tutorial",
          "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
-		 "views":0},
+		 "views": 0},
         {"title": "Django Rocks",
          "url": "http://www.djangorocks.com/",
 		 "views": 0},
@@ -33,14 +36,14 @@ def populate():
     other_pages = [
         {"title": "Bottle",
          "url": "http://bottlepy.org/docs/dev/",
-		 "views":50},
+		 "views": 50},
         {"title": "Flask",
          "url": "http://flask.pocoo.org",
-		 "views":10} ]
+		 "views": 10} ]
 
-    cats = {"Python": {"pages": python_pages,"views":128, "likes":64},
-            "Django": {"pages": django_pages, "views": 64, "likes":32},
-            "Other Frameworks": {"pages": other_pages,"view": 32,"likes":16}}
+    cats = {"Python": {"pages": python_pages,"views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages,"view": 32,"likes": 16}}
 
     for cat, cat_data in cats.items():
         c = add_cat(cat)
@@ -53,19 +56,19 @@ def populate():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
 
-
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url=url
-    p.views=views
+    p.url = url
+    p.views = views
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
-
     # Start execution here!
 if __name__ == '__main__':
     print("Starting Rango population script...")
